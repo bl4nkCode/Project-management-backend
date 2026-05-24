8c878b461a25468773725f46f6959b5abddda64e
bryan <canizarbryanangelo@gmail.com>
2026-05-24 17:09:51 +0800

    Task API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\ProjectController;
+use App\Http\Controllers\API\TaskController;
 use App\Http\Controllers\API\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,3 +14,5 @@ Route::get('/user', function (Request $request) {
 Route::get('/test', [TestController::class, 'index']);
 
 Route::apiResource('projects', ProjectController::class);
+
+Route::apiResource('tasks', TaskController::class);
