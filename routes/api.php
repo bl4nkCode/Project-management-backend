ce4e3eabe270f5aa122c7ff8f94a10c3388ca292
bryan <canizarbryanangelo@gmail.com>
2026-05-26 05:50:57 +0800

    Implement authentication API with registration, login, and logout functionality

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\ProjectController;
 use App\Http\Controllers\API\TaskController;
 use App\Http\Controllers\API\TestController;
@@ -13,6 +14,10 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/test', [TestController::class, 'index']);
 
-Route::apiResource('projects', ProjectController::class);
+Route::apiResource('projects', ProjectController::class)->middleware('auth:sanctum');
+Route::apiResource('tasks', TaskController::class)->middleware('auth:sanctum');
 
-Route::apiResource('tasks', TaskController::class);
+// Register API
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']); // Login API
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum'); // Logout API
